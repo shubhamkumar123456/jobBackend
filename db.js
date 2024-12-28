71b2ba0169f9e61d4d83daabc03b1cd1742b42c4
shubham kumar <clboy768@gmail.com>
2024-12-28 10:57:22 +0530

    updated mongodb atlas

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 
 const connectToDB = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/JobPortal')
+    mongoose.connect(`mongodb+srv://clboy768:${process.env.MONGO_PASSWORD}@jobportal.funkw.mongodb.net/?retryWrites=true&w=majority&appName=jobPortal`)
     .then(() => console.log('connected to mongodb successfully'))
     .catch(()=>console.log('error in connecting mongodb'))
 }
